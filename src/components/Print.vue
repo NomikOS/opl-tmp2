390691b453533eaa721ae694a3ea6ea12e5fe0be
Igor Parra <usuario3@gmail.com>
2016-07-17 13:53:54 -0300

    new plugin

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -16,16 +16,16 @@
       </a>
     </li>
     <li>
-      <a @click="print()" class="waves-effect waves-light">
+      <a @click="find()" class="waves-effect waves-light">
         <div class="ac25-main-menu-content">
-          <p>orden <br/> interna</p>
+          <p>find()</p>
         </div>
       </a>
     </li>
     <li>
       <a @click="print()" class="waves-effect waves-light">
         <div class="ac25-main-menu-content">
-          <p>orden <br/> cliente</p>
+          <p>print()</p>
         </div>
       </a>
     </li>
@@ -54,32 +54,51 @@
 </template>
 
 <script>
+export default {
 
-  export default {
+  name: 'Print',
 
-    name: 'Print',
-
-    data() {
-      return {
+  data() {
+    return {
       // user: director.user
     }
   },
 
   methods: {
 
+    // print() {
+    //   cordova.plugins.zbtprinter.print("^XA^FO10,10^AFN,26,13^FDWelcome to the new world order, Pak!^FS^XZ",
+    //     function(success) { 
+    //       console.info("zbtprinter ok !!!!!!!!!"); 
+    //     }, function(fail) { 
+    //       console.info(fail, 'zbtprinter fail !!!!!!!!!'); 
+    //     });
+    // },
+    find() {
+      cordova.plugins.zbtprinter.find( function( mac ) {
+        alert( mac );
+      }, function( fail ) {
+        alert( fail );
+      } )
+    },
+
     print() {
-      cordova.plugins.zbtprinter.print("^XA^FO10,10^AFN,26,13^FDWelcome to the new world order, Pak!^FS^XZ",
-        function(success) { 
-          console.info("zbtprinter ok !!!!!!!!!"); 
-        }, function(fail) { 
-          console.info(fail, 'zbtprinter fail !!!!!!!!!'); 
+      cordova.plugins.zbtprinter.print( "AC:3F:A4:56:66:EC", "! U1 setvar "
+        device.languages " "
+        line_print "\r\nTEXT ***Welcome to the new world order, Pak!***\r\nPRINT\r\n",
+        function( success ) {
+          alert( "Print ok" );
+        },
+        function( fail ) {
+          alert( fail );
         }
-        );
-    }
+      );
+    },
+
   },
 
-  ready(){
-    console.info('Print is ready ===================================');
-  }  
+  ready() {
+    console.info( 'Print is ready ===================================' );
+  }
 }
 </script>
