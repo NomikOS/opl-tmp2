7f22e80ba878e7688b80a8b6ff49cf59374bd3a8
Igor Parra <usuario3@gmail.com>
2016-08-03 14:42:10 -0300

    automatic commit

--- src/components/Scan.vue
+++ src/components/Scan.vue
@@ -17,8 +17,8 @@
         <li>
           <a>
             <div class="ac25-scan-list-content">
-              <span class="ac25-sclanlist-scan-id" v-if="addressType != 'delivery'">{{item_id_info}}</span>
-              <span class="ac25-sclanlist-scan-id" v-if="addressType == 'delivery' && item.qr_id > 0">{{item_id_info}}</span>
+              <span class="ac25-sclanlist-scan-id" v-if="addressType != 'delivery'">{{{item_id_info}}}</span>
+              <span class="ac25-sclanlist-scan-id" v-if="addressType == 'delivery' && item.qr_id > 0">{{{item_id_info}}}</span>
             </div>
           </a>
         </li>
