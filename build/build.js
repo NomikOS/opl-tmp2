458ebe720f46273b6b5522a91fc928abca70ba57
Igor Parra <usuario3@gmail.com>
2016-10-14 20:28:50 -0300

    disabling backbutton and degugging modal

--- build/build.js
+++ build/build.js
@@ -16815,37 +16815,37 @@
 	//        <p class="left clearfix ac25-subtitle"> Orden {{order.special_id}} </p>
 	//        <img class="ac25-top-right-hand ac25-z-1" src="html/images/hand-black.png" v-link="'call'" />
 	//
-	//         <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
-	//           <li> Cargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
-	//           <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
-	//         </ul>
+	//        <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
+	//         <li> Cargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
+	//         <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
+	//       </ul>
 	//
-	//         <div class="clearfix"></div>
-	//           <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
-	//         </div><!-- end content-inner-holder -->
-	//       </div><!-- end container -->
+	//       <div class="clearfix"></div>
+	//       <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
+	//     </div><!-- end content-inner-holder -->
+	//   </div><!-- end container -->
 	//
-	//       <div class="ac25-content-inner-holder ac25-min-height-200" v-if="addressType == 'delivery'">
-	//        <h4 class="ac25-top-red-text">DESCARGAR<br />EL CAMION</h4>
-	//        <p class="left clearfix ac25-subtitle"> Orden {{order.special_id}} </p>
-	//        <img class="ac25-top-right-hand ac25-z-1" src="html/images/hand-black.png" v-link="'call'" />
+	//   <div class="ac25-content-inner-holder ac25-min-height-200" v-if="addressType == 'delivery'">
+	//    <h4 class="ac25-top-red-text">DESCARGAR<br />EL CAMION</h4>
+	//    <p class="left clearfix ac25-subtitle"> Orden {{order.special_id}} </p>
+	//    <img class="ac25-top-right-hand ac25-z-1" src="html/images/hand-black.png" v-link="'call'" />
 	//
-	//         <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
-	//           <li> Descargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
-	//           <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
-	//         </ul>
+	//    <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
+	//     <li> Descargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
+	//     <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
+	//   </ul>
 	//
-	//         <div class="clearfix"></div>
-	//           <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
-	//         </div><!-- end content-inner-holder -->
-	//       </div><!-- end container -->
+	//   <div class="clearfix"></div>
+	//   <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
+	// </div><!-- end content-inner-holder -->
+	// </div><!-- end container -->
 	//
-	//       <footer class="ac25-content-footer">
-	//         <a onclick="window.history.back()" class="ac25-half-black left waves-effect waves-light">volver</a>
-	//         <a @click="finishOrder()" class="ac25-half-red right waves-effect waves-light">terminar</a>
-	//       </footer><!-- end footer -->
+	// <footer class="ac25-content-footer">
+	//   <a onclick="window.history.back()" class="ac25-half-black left waves-effect waves-light">volver</a>
+	//   <a @click="finishOrder()" class="ac25-half-red right waves-effect waves-light">terminar</a>
+	// </footer><!-- end footer -->
 	//
-	//   </div><!-- end content-global -->
+	// </div><!-- end content-global -->
 	// </template>
 	//
 	// <script>
@@ -16880,11 +16880,10 @@
 	      var addressType = this.addressType;
 
 	      _ModalWait2.default.showIt(true, 'finish-shipment');
-
-	      setTimeout(function () {
-	        _ModalWait2.default.showIt(false);
-	      }, 3000);
-	      return;
+	      // setTimeout(function(){
+	      //   ModalWait.showIt( false )
+	      // }, 3000)
+	      // return;
 
 	      this.$http.post(ORDER_URL + '/finish-shipment', {
 	        order_id: order_id,
@@ -17025,7 +17024,7 @@
 /* 72 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = "\n  <header-user-data></header-user-data>\n  <modal-wait></modal-wait>\n\n  <div class=\"ac25-content-global\">\n    <div class=\"container\">\n\n      <div class=\"ac25-content-inner-holder ac25-min-height-200\" v-if=\"addressType == 'pickup'\">\n       <h4 class=\"ac25-top-red-text\">CARGAR EL CAMION</h4>\n       <p class=\"left clearfix ac25-subtitle\"> Orden {{order.special_id}} </p>\n       <img class=\"ac25-top-right-hand ac25-z-1\" src=\"" + __webpack_require__(61) + "\" v-link=\"'call'\" />\n\n        <ul class=\"ac25-red-list clearfix ac25-fleft ac25-mtop60\">\n          <li> Cargue los <span class=\"ac25-large-font\">{{order.items_amount}}</span> bultos.</li>\n          <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>\n        </ul>\n\n        <div class=\"clearfix\"></div>\n          <a @click=\"print('items-list')\" class=\"ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light\"> <img src=\"" + __webpack_require__(73) + "\" class=\"left\" /><span>imprimir listado de bultos</span> </a>\n        </div><!-- end content-inner-holder -->\n      </div><!-- end container -->\n\n      <div class=\"ac25-content-inner-holder ac25-min-height-200\" v-if=\"addressType == 'delivery'\">\n       <h4 class=\"ac25-top-red-text\">DESCARGAR<br />EL CAMION</h4>\n       <p class=\"left clearfix ac25-subtitle\"> Orden {{order.special_id}} </p>\n       <img class=\"ac25-top-right-hand ac25-z-1\" src=\"" + __webpack_require__(61) + "\" v-link=\"'call'\" />\n\n        <ul class=\"ac25-red-list clearfix ac25-fleft ac25-mtop60\">\n          <li> Descargue los <span class=\"ac25-large-font\">{{order.items_amount}}</span> bultos.</li>\n          <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>\n        </ul>\n\n        <div class=\"clearfix\"></div>\n          <a @click=\"print('items-list')\" class=\"ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light\"> <img src=\"" + __webpack_require__(73) + "\" class=\"left\" /><span>imprimir listado de bultos</span> </a>\n        </div><!-- end content-inner-holder -->\n      </div><!-- end container -->\n\n      <footer class=\"ac25-content-footer\">\n        <a onclick=\"window.history.back()\" class=\"ac25-half-black left waves-effect waves-light\">volver</a>\n        <a @click=\"finishOrder()\" class=\"ac25-half-red right waves-effect waves-light\">terminar</a>\n      </footer><!-- end footer -->\n\n  </div><!-- end content-global -->\n";
+	module.exports = "\n  <header-user-data></header-user-data>\n  <modal-wait></modal-wait>\n\n  <div class=\"ac25-content-global\">\n    <div class=\"container\">\n\n      <div class=\"ac25-content-inner-holder ac25-min-height-200\" v-if=\"addressType == 'pickup'\">\n       <h4 class=\"ac25-top-red-text\">CARGAR EL CAMION</h4>\n       <p class=\"left clearfix ac25-subtitle\"> Orden {{order.special_id}} </p>\n       <img class=\"ac25-top-right-hand ac25-z-1\" src=\"" + __webpack_require__(61) + "\" v-link=\"'call'\" />\n\n       <ul class=\"ac25-red-list clearfix ac25-fleft ac25-mtop60\">\n        <li> Cargue los <span class=\"ac25-large-font\">{{order.items_amount}}</span> bultos.</li>\n        <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>\n      </ul>\n\n      <div class=\"clearfix\"></div>\n      <a @click=\"print('items-list')\" class=\"ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light\"> <img src=\"" + __webpack_require__(73) + "\" class=\"left\" /><span>imprimir listado de bultos</span> </a>\n    </div><!-- end content-inner-holder -->\n  </div><!-- end container -->\n\n  <div class=\"ac25-content-inner-holder ac25-min-height-200\" v-if=\"addressType == 'delivery'\">\n   <h4 class=\"ac25-top-red-text\">DESCARGAR<br />EL CAMION</h4>\n   <p class=\"left clearfix ac25-subtitle\"> Orden {{order.special_id}} </p>\n   <img class=\"ac25-top-right-hand ac25-z-1\" src=\"" + __webpack_require__(61) + "\" v-link=\"'call'\" />\n\n   <ul class=\"ac25-red-list clearfix ac25-fleft ac25-mtop60\">\n    <li> Descargue los <span class=\"ac25-large-font\">{{order.items_amount}}</span> bultos.</li>\n    <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>\n  </ul>\n\n  <div class=\"clearfix\"></div>\n  <a @click=\"print('items-list')\" class=\"ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light\"> <img src=\"" + __webpack_require__(73) + "\" class=\"left\" /><span>imprimir listado de bultos</span> </a>\n</div><!-- end content-inner-holder -->\n</div><!-- end container -->\n\n<footer class=\"ac25-content-footer\">\n  <a onclick=\"window.history.back()\" class=\"ac25-half-black left waves-effect waves-light\">volver</a>\n  <a @click=\"finishOrder()\" class=\"ac25-half-red right waves-effect waves-light\">terminar</a>\n</footer><!-- end footer -->\n\n</div><!-- end content-global -->\n";
 
 /***/ },
 /* 73 */
--- src/components/LoadVehicle.vue
+++ src/components/LoadVehicle.vue
@@ -10,37 +10,37 @@
        <p class="left clearfix ac25-subtitle"> Orden {{order.special_id}} </p>
        <img class="ac25-top-right-hand ac25-z-1" src="html/images/hand-black.png" v-link="'call'" />
 
-        <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
-          <li> Cargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
-          <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
-        </ul>
+       <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
+        <li> Cargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
+        <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
+      </ul>
 
-        <div class="clearfix"></div>
-          <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
-        </div><!-- end content-inner-holder -->
-      </div><!-- end container -->
+      <div class="clearfix"></div>
+      <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
+    </div><!-- end content-inner-holder -->
+  </div><!-- end container -->
 
-      <div class="ac25-content-inner-holder ac25-min-height-200" v-if="addressType == 'delivery'">
-       <h4 class="ac25-top-red-text">DESCARGAR<br />EL CAMION</h4>
-       <p class="left clearfix ac25-subtitle"> Orden {{order.special_id}} </p>
-       <img class="ac25-top-right-hand ac25-z-1" src="html/images/hand-black.png" v-link="'call'" />
+  <div class="ac25-content-inner-holder ac25-min-height-200" v-if="addressType == 'delivery'">
+   <h4 class="ac25-top-red-text">DESCARGAR<br />EL CAMION</h4>
+   <p class="left clearfix ac25-subtitle"> Orden {{order.special_id}} </p>
+   <img class="ac25-top-right-hand ac25-z-1" src="html/images/hand-black.png" v-link="'call'" />
 
-        <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
-          <li> Descargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
-          <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
-        </ul>
+   <ul class="ac25-red-list clearfix ac25-fleft ac25-mtop60">
+    <li> Descargue los <span class="ac25-large-font">{{order.items_amount}}</span> bultos.</li>
+    <li> Una vez que este listo para pasar a la siguiente orden, persione terminar.</li>
+  </ul>
 
-        <div class="clearfix"></div>
-          <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
-        </div><!-- end content-inner-holder -->
-      </div><!-- end container -->
+  <div class="clearfix"></div>
+  <a @click="print('items-list')" class="ac25-print-button ac25-mbottom50 clearfix waves-effect waves-light"> <img src="html/images/print.png" class="left" /><span>imprimir listado de bultos</span> </a>
+</div><!-- end content-inner-holder -->
+</div><!-- end container -->
 
-      <footer class="ac25-content-footer">
-        <a onclick="window.history.back()" class="ac25-half-black left waves-effect waves-light">volver</a>
-        <a @click="finishOrder()" class="ac25-half-red right waves-effect waves-light">terminar</a>
-      </footer><!-- end footer -->
+<footer class="ac25-content-footer">
+  <a onclick="window.history.back()" class="ac25-half-black left waves-effect waves-light">volver</a>
+  <a @click="finishOrder()" class="ac25-half-red right waves-effect waves-light">terminar</a>
+</footer><!-- end footer -->
 
-  </div><!-- end content-global -->
+</div><!-- end content-global -->
 </template>
 
 <script>
@@ -81,11 +81,10 @@
         var addressType = this.addressType
 
         ModalWait.showIt( true, 'finish-shipment' )
-
-setTimeout(function(){
-  ModalWait.showIt( false )
-}, 3000)
-        return;
+        // setTimeout(function(){
+        //   ModalWait.showIt( false )
+        // }, 3000)
+        // return;
 
 
         this.$http.post( ORDER_URL + '/finish-shipment', {
