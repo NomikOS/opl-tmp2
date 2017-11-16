ec1ffb3ff022de883da3b3f77049af172782f84f
Igor Parra <igor.parra@agente.cl>
2017-11-16 15:12:48 -0300

    Con ingreso de pagos 2

--- src/services/upload.js
+++ src/services/upload.js
@@ -29,7 +29,7 @@ export default {
       console.info( navigator )
       console.info( navigator.camera )
       console.info( device )
-      console.info( navigator.device.capture )
+      console.info( navigator.device.capture )  
 
       if ( typeof navigator.device === 'undefined' ) {
         return alert( 'Camera device needed', 'No hay acceso a la cámara.' )
